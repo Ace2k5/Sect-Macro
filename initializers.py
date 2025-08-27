306ea81244b125cef280cbcb7b563cb6a60f9c46
Ace2k5 <acebastianbotones@gmail.com>
2025-08-27 22:22:14 +0800

    Initialzers update

--- initializers.py
+++ initializers.py
@@ -2,13 +2,19 @@ import win32gui
 import win32con
 import time
 
+resolutions = {
+    "Roblox": (800, 600)
+}
+# Current scope
+width, height = resolutions["Roblox"]
+
 def window_info():
     try:
         hwnd = win32gui.FindWindow(None, "Roblox")
         win32gui.ShowWindow(hwnd, win32con.SW_RESTORE) #Grabs HWND and uses SW_RESTORE to prevent minimization
         rect = win32gui.GetWindowRect(hwnd)
         x, y = rect[0], rect[1]
-        win32gui.MoveWindow(hwnd, x, y, 800, 600, True) #Makes sure 800x600 is constant. Grabs x and y from rect
+        win32gui.MoveWindow(hwnd, x, y, width, height, True) #Makes sure 800x600 is constant. Grabs x and y from rect
         win32gui.SetForegroundWindow(hwnd) #Makes sure roblox handle is always the focus
         return win32gui.GetWindowRect(hwnd) #Returns left, top, right and bottom
     except Exception as e:
