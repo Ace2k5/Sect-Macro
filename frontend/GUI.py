f1e9e9d4822280adfd2a9ca5b0f481477831c263
Ace2k5 <acebastianbotones@gmail.com>
2025-08-29 23:43:42 +0800

    gui test

--- frontend/GUI.py
+++ frontend/GUI.py
@@ -2,24 +2,22 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtGui import QFont
 import pyautogui
+from backend import initializers
 #Still testing pyqt
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.screen_res = pyautogui.size()
+        self.game_res = initializers.resolutions.get("Roblox")
         self.res = self.resolution_mid()
-        self.setGeometry(self.res[0], self.res[1], 800, 600)
+        self.setGeometry(self.res[0], self.res[1], (self.game_res[0] + 200), (self.game_res[1] + 100))
         self.setWindowTitle("Sect v0.0.1")
-        
-        label = QLabel("Hello", self)
-        label.setFont(QFont("Arial"))
-        label.setGeometry(0, 0, 1000, 600)
 
         
     def resolution_mid(self):
         screen_resolution = self.screen_res
-        x = (screen_resolution.width - 800) // 2
-        y = (screen_resolution.height- 600) // 2
+        x = (screen_resolution.width - 1000) // 2
+        y = (screen_resolution.height- 700) // 2
         middle_screen = (x, y)
         return middle_screen
 
