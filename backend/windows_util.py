13a36575bafb28c44ef5a2effc9fcae5775a360f
Ace2k5 <acebastianbotones@gmail.com>
2025-09-11 22:55:22 +0800

    another refactor

--- backend/windows_util.py
+++ backend/windows_util.py
@@ -1,13 +1,17 @@
 import pyautogui
+import win32gui
 from frontend import guardians
 
-class windowsUtils():
-    def __init__(self):
-          self.resolutionMid()
+def initWindow(title: str):
+    hwnd = win32gui.FindWindow(None, title)
+    while hwnd is None:
+        print(f"Cannot find {title}, most likely not opened or a rare bug. Redoing...")
+        hwnd = win32gui.FindWindow(None, title)
+    return hwnd
 
-    def resolutionMid(self):
-            screen_resolution = pyautogui.size()
-            x = (screen_resolution.width - 1000) // 2 # 1000x700 since roblox is at 800x600
-            y = (screen_resolution.height- 700) // 2 # this 1000x700 is primarily for the qt application
-            middle_screen = (x, y)
-            return middle_screen
+def resolutionMid(window_width: int, window_height: int):
+    screen_resolution = pyautogui.size()
+    x = (screen_resolution.width - window_width) // 2 # 1000x700 since roblox is at 800x600
+    y = (screen_resolution.height- window_height) // 2 # this 1000x700 is primarily for the qt application
+    middle_screen = (x, y)
+    return middle_screen
--- frontend/guardians.py
+++ frontend/guardians.py
@@ -12,6 +12,7 @@ TITLE = "Sect v0.0.1"
 class Guardians(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.game_res = initializers.resolutions.get("Roblox")
         self.mainWindow()
         self.layout = QVBoxLayout()
         self.main_widget = QWidget()
@@ -19,7 +20,6 @@ class Guardians(QMainWindow):
         self.setCentralWidget(self.main_widget)
         self.initLabels()
         self.initWindow()
-        self.game_res = initializers.resolutions.get("Roblox")
 
     def mainWindow(self):
         self.res = windows_util.resolutionMid()
@@ -36,7 +36,7 @@ class Guardians(QMainWindow):
         self.layout.addWidget(title, alignment=Qt.AlignLeft | Qt.AlignTop)
     
     def initWindow(self):
-        self.hwnd = win32gui.FindWindow(None, "Roblox")
+        self.hwnd = windows_util.initWindow("Roblox")
         self.container = QWidget(self)
         win32gui.SetParent(self.hwnd, int(self.container.winId())) # set qt window as parent application and roblox as child, so if user decides to move qt application roblox also moves
         self.layout.addWidget(self.container)
@@ -47,6 +47,7 @@ class Guardians(QMainWindow):
         game_width = self.game_res[0]
         game_height = self.game_res[1]
         self.container.setFixedSize(game_width, game_height)
-        win32gui.MoveWindow(self.hwnd, 0, 0, game_width, game_height, True) #Makes sure 800x600 is constant. Grabs x and y from rect
+        x_coord, y_coord = windows_util.resolutionMid(game_width, game_height)
+        win32gui.MoveWindow(self.hwnd, x_coord, y_coord, game_width, game_height, True) #Makes sure 800x600 is constant. Grabs x and y from rect
         self.coordinates = (self.res[0], self.res[1], self.game_res[0], self.game_res[1])
         return self.coordinates
